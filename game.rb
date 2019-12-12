b8aca41a9ab83590d6fe40175a18fcb23f05a4b5
Hebron George <hebrontgeorge@gmail.com>
2019-12-11 20:47:53 -0600

    start from 'scratch'

--- game.rb
+++ game.rb
@@ -17,14 +17,8 @@ end
 
 class Game < Gosu::Window
   def initialize
-    super 1280, 480
+    super 640, 480
     self.caption = "Simmy Says"
-
-    @count = 0
-    @count_text = Gosu::Font.new(100)
-    @score = 0
-    @score_text = Gosu::Font.new(100)
-    @click_count = 0
 
     @background_image = Gosu::Image.new("assets/space.png", :tileable => true)
   end
@@ -32,34 +26,13 @@ class Game < Gosu::Window
   def update
     # called 60 times per second by default, and should contain the main
     # game logic, such as moving objects around, or testing for collisions.
-    @count = Random.rand(5000)
   end
 
   def draw
     # usually called 60 times per second, but may be skipped for performance reasons.
     # It should contain code to redraw the whole scene, but no game logic.
     @background_image.draw(0,0,0)
-    @background_image.draw(640,0,0)
-    @count_text.draw_text("Try yer' luck: #{@count}", 10, 10, 0)
-    @score_text.draw_text("Score: #{@score}", 10, 210, 0)
   end
-
-  def button_down(id)
-    case id
-      when Gosu::KbEscape
-        close
-      when Gosu::MsLeft
-        update_score!
-    end
-  end
-
-  private
-    def update_score!
-      return if @click_count > 4
-      @click_count += 1
-      @score += @count
-    end
-
 end
 
 Game.new.show
